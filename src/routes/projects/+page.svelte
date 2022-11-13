5a1d4142030c865f6206946fca2f6f156c240f5d
MrZCookie <zacharyking08@hotmail.com>
2022-11-12 18:20:07 -0600

    Added NoCommandPrefixes to Projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -23,4 +23,19 @@
 			<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Sveltekit</span>
 		</div>
 	</div>
+	<div class="max-w-sm rounded overflow-hidden shadow-lg bg-slate-800 cursor-pointer mx-10 my-16 justify-center transition duration-500 hover:scale-105 hover:shadow-xl" on:click={() => (window.location.replace("https://github.com/MrZCookies/NoCommandPrefixes"))}>
+		<img class="w-full" src="/nocommandprefixes.png" alt="MrZCookie's Website">
+		<div class="px-6 py-4">
+			<div class="text-xl mb-2 text-gray-100">
+				NoCommandPrefixes
+			</div>
+			<p class="text-gray-400 text-base">
+				A plugin that is used to remove command's fallback prefixes to not reveal your plugins or if you just hate having them on your server.
+			</p>
+		</div>
+		<div class="px-6 pt-4 pb-2">
+			<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Java</span>
+			<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Bukkit API</span>
+		</div>
+	</div>
 </div>
