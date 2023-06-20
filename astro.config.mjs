4511cc26327be8740410ce87d67b65ab980a3e4b
MrZCookie <91168951+MrZCookies@users.noreply.github.com>
2023-06-19 23:51:24 -0500

    Convert sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [svelte(), tailwind()]
+  site: "https://mrzcookie.com",
+  integrations: [svelte(), tailwind(), sitemap()]
 });
